10f478a12a606a336571cd7244f60a4a512d4f52
Chris Georgen <chrisjgeorgen@gmail.com>
2024-02-22 12:19:44 -0600

    improved carbon_dev.sol to include transfer function and updated metadata location

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,7 +4,7 @@ async function main() {
 
     const contractAddress = "0x2580571A6a68f05a7EC1E535C2e539aB7dd07848";
     const recieverAddress = "0x5CBaF6a447d44d52bE4b6e6a7241e067b614Da49";
-    const baseURI = "https://gateway.pinata.cloud/ipfs/QmPNVWssQVgCJT96nuUJDYdaLE2rTZ6U1k3FDtLAheo99W";
+    const baseURI = "https://jade-near-wasp-97.mypinata.cloud/ipfs/QmSwxPaCVq9oem7KBe6TVqqsHjuKymwAbRsJsfcUPu3eEG";
     const serializiedCredits = await hre.ethers.getContractAt("SerializiedCredits", contractAddress);
     
     const mintTokens = await serializiedCredits.mint(recieverAddress, 5, baseURI);
