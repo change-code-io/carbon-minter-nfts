a9d31e6e2ca7650ef0c3413c15de1c6b800e6c5d
Chris Georgen <chrisjgeorgen@gmail.com>
2024-02-22 11:26:02 -0600

    clean up before making changing repo visibility to public

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,26 @@
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
-const { API_URL, PRIVATE_KEY_MINTER } = process.env;
+const { API_URL, PRIVATE_KEY_MINTER, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.20",
   defaultNetwork: "mumbai",
   networks: {
     hardhat: {},
-    mumbai: {
+    sepolia: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY_MINTER}`],
     },
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  }
 };
