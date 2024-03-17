24fa7be8f051f69bb8ca20533ad4825a0ebde8de
Chris Georgen <c-georgen@users.noreply.github.com>
2024-03-17 16:42:36 -0500

    Closes CHA-64 and CHA-63 and cleans project specifics out from main

--- scripts/burn.js
+++ scripts/burn.js
@@ -2,8 +2,8 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const contractAddress = "0x14d82184f8CABECa455b22c2ea8d7d4Db67BD69E";
-    const tokenIDs = [0, 2, 3, 4];
+    const contractAddress = "";
+    const tokenIDs = [];
     const carbon = await hre.ethers.getContractAt("Carbon", contractAddress);
     
     const burnTokens = await carbon.burn_plus(tokenIDs);
--- scripts/mint.js
+++ scripts/mint.js
@@ -2,17 +2,17 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const contractAddress = "0x14d82184f8CABECa455b22c2ea8d7d4Db67BD69E";
+    const contractAddress = "";
 
-    const carbon_minter = "0x204e07573052aa523f2662F98A3057791B19208C";
-    const carbon_recipient_1 = "0x4B214516602e8541550b7e0700e527ADf452C42C";
+    const carbon_minter = "";
+    const carbon_recipient_1 = "";
 
-    const baseURI = "https://best-strengthening-495962.framer.app/methodologies/soil-carbon-methodology";
+    const baseURI = "https://changecode.io";
     const carbon = await hre.ethers.getContractAt("Carbon", contractAddress);
     
     const mintTokens = 
-    await carbon.mint_plus(carbon_recipient_1, 4364, baseURI, "BCR-US-00002-00001-04364-BCSC-BCVC-EO-AMSIIIR-EBG001-2023-05-10");
-    await carbon.mint_plus(carbon_recipient_1, 6817, baseURI, "BCR-US-00004-00001-06817-BCSC-BCVC-LVI-AMSIIIR-EBG001-2024-03-11");
+    await carbon.mint_plus(carbon_recipient_1, 0, baseURI, "");
+    await carbon.mint_plus(carbon_recipient_1, 0, baseURI, "");
 
 
     console.log(`Transaction Hash: https://mumbai.polygonscan.com/tx/${mintTokens.hash}`);
