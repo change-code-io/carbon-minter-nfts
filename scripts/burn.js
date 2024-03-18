92853ea6efb12754ca2326a039faa2d6d133d7ea
Chris Georgen <c-georgen@users.noreply.github.com>
2024-03-18 13:29:36 -0500

    ran tests to determine cost of token burning on Polygon

--- scripts/burn.js
+++ scripts/burn.js
@@ -2,8 +2,8 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const contractAddress = "0x14d82184f8CABECa455b22c2ea8d7d4Db67BD69E";
-    const tokenIDs = [0, 2, 3, 4];
+    const contractAddress = "0x5CBaF6a447d44d52bE4b6e6a7241e067b614Da49";
+    const tokenIDs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
     const carbon = await hre.ethers.getContractAt("Carbon", contractAddress);
     
     const burnTokens = await carbon.burn_plus(tokenIDs);
--- scripts/mint.js
+++ scripts/mint.js
@@ -2,17 +2,16 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const contractAddress = "0x14d82184f8CABECa455b22c2ea8d7d4Db67BD69E";
+    const contractAddress = "0x7b77B0683DDdD305ef026667474a985C099eE3cc";
 
-    const carbon_minter = "0x204e07573052aa523f2662F98A3057791B19208C";
-    const carbon_recipient_1 = "0x4B214516602e8541550b7e0700e527ADf452C42C";
+    const carbon_minter = "0x5CBaF6a447d44d52bE4b6e6a7241e067b614Da49";
+    const carbon_recipient_1 = "0x5CBaF6a447d44d52bE4b6e6a7241e067b614Da49";
 
     const baseURI = "https://best-strengthening-495962.framer.app/methodologies/soil-carbon-methodology";
     const carbon = await hre.ethers.getContractAt("Carbon", contractAddress);
     
     const mintTokens = 
-    await carbon.mint_plus(carbon_recipient_1, 4364, baseURI, "BCR-US-00002-00001-04364-BCSC-BCVC-EO-AMSIIIR-EBG001-2023-05-10");
-    await carbon.mint_plus(carbon_recipient_1, 6817, baseURI, "BCR-US-00004-00001-06817-BCSC-BCVC-LVI-AMSIIIR-EBG001-2024-03-11");
+    await carbon.mint_plus(carbon_recipient_1, 1000, baseURI, "BCR-US-00002-00001-04364-BCSC-BCVC-EO-AMSIIIR-EBG001-2023-05-10");
 
 
     console.log(`Transaction Hash: https://mumbai.polygonscan.com/tx/${mintTokens.hash}`);
