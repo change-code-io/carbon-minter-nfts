c609d281fca97eef7439506c75a621241246805a
Chris Georgen <c-georgen@users.noreply.github.com>
2024-03-25 14:55:44 -0500

    retested burning functionality

--- scripts/burn.js
+++ scripts/burn.js
@@ -2,8 +2,8 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const contractAddress = "0x5CBaF6a447d44d52bE4b6e6a7241e067b614Da49";
-    const tokenIDs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+    const contractAddress = "0x7b77B0683DDdD305ef026667474a985C099eE3cc";
+    const tokenIDs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
     const carbon = await hre.ethers.getContractAt("Carbon", contractAddress);
     
     const burnTokens = await carbon.burn_plus(tokenIDs);
